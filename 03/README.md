b57fb3e5914a5107a8f7c3259aa5084080482aa3
wpower12 <willpowe@gmail.com>
2015-12-03 21:10:52 -0500

    Day 3

---
+++ 03/README.md
@@ -0,0 +1,8 @@
+# Santa and ROBO SANTA???
+
+Tried in C.  Pretty verbose.  But fun.  The robo-santa one was a lot of
+lazy copy and paste, but it works.
+
+Basically read the file to size a grid, then read it again to fill in when
+gifts were dropped off.  This is fun, because I also get stuff like what
+house gets the most presents for free from the array implementation.
---
+++ 03/robosanta.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#define MAX_INT 999999
+#define MIN_INT -999999
+void main(){
+  FILE *fp;
+  char *fn = "moves.txt";
+
+  //Count the size of the required array
+  int xmin=MAX_INT, xmax=MIN_INT, ymin=MAX_INT, ymax=MIN_INT;
+  int x = 0, y = 0;
+  int robo_x = 0, robo_y = 0;
+  fp = fopen( fn, "r" );
+  char c;
+  int flag = 0;
+  while( (c = fgetc(fp)) != EOF ){
+    if(flag == 0){
+    switch( c ){
+      case '>':
+      x++;
+      break;
+      case '<':
+      x--;
+      break;
+      case '^':
+      y++;
+      break;
+      case 'v':
+      y--;
+      break;
+      default:
+      break;
+    }
+    if( x > xmax ){
+      xmax = x;
+    } else if ( x < xmin ){
+      xmin = x;
+    }
+    if( y > ymax ){
+      ymax = y;
+    } else if ( y < ymin ){
+      ymin = y;
+    }
+    flag = 1;
+  } else {
+    switch( c ){
+      case '>':
+      robo_x++;
+      break;
+      case '<':
+      robo_x--;
+      break;
+      case '^':
+      robo_y++;
+      break;
+      case 'v':
+      robo_y--;
+      break;
+      default:
+      break;
+    }
+    if( robo_x > xmax ){
+      xmax = robo_x;
+    } else if ( robo_x < xmin ){
+      xmin = robo_x;
+    }
+    if( robo_y > ymax ){
+      ymax = robo_y;
+    } else if ( robo_y < ymin ){
+      ymin = robo_y;
+    }
+    flag = 0;
+  }
+  }
+  fclose(fp);
+
+  //Now that we know how big of a grid santa is working on, we can do some
+  //stuff with arrays.  yaaaaaaaaaaaay
+  int x_offset = xmin;
+  int y_offset = ymin;
+  int x_size = xmax - x_offset + 1;
+  int y_size = ymax - y_offset + 1;
+  printf("x: %d %d y: %d %d\n", xmin, xmax, ymin, ymax);
+  printf("x: %d y: %d\n", x_size, y_size);
+  fp = fopen( fn, "r" );
+  int houses[ x_size ][ y_size ];
+  int i, j;
+  for( i = 0; i < x_size; i++ ){
+    for( j = 0; j < y_size; j++ ){
+      houses[i][j] = 0;
+    }
+  }
+
+  int total = 1;
+  x = 0 - x_offset;
+  y = 0 - y_offset;
+  robo_x = x, robo_y = y;
+  houses[x][y] = 2;
+  flag = 0;
+  while((c = fgetc(fp)) != EOF){
+    if(flag == 0){
+      switch( c ){
+        case '>':
+        x++;
+        break;
+        case '<':
+        x--;
+        break;
+        case '^':
+        y++;
+        break;
+        case 'v':
+        y--;
+        break;
+        default:
+        break;
+      }
+      if( houses[x][y] == 0 ){
+        total++;
+      }
+      houses[x][y]++;
+      flag = 1;
+    } else {
+      switch( c ){
+        case '>':
+        robo_x++;
+        break;
+        case '<':
+        robo_x--;
+        break;
+        case '^':
+        robo_y++;
+        break;
+        case 'v':
+        robo_y--;
+        break;
+        default:
+        break;
+      }
+      if( houses[robo_x][robo_y] == 0 ){
+        total++;
+      }
+      houses[robo_x][robo_y]++;
+      flag = 0;
+    }
+  }
+  printf("Houses: %d\n", total);
+}
